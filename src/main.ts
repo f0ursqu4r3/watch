827745da05ef45451fd76e1ee91922487e57db82
kyle dougan <kyle.dougan@e-o.solutions>
2026-06-05 19:47:26 -1000

    feat(i18n): install vue-i18n and init locale at startup

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { i18n } from './i18n'
+import { initLocale } from './composables/useLocale'
 
-createApp(App).mount('#app')
+initLocale()
+createApp(App).use(i18n).mount('#app')
